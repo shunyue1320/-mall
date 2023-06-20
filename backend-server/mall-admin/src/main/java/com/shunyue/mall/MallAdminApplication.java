b6d96011f526429387a4f2457e876a1bba6ba57f
shunyue1320 <tsunyue1320@gmail.com>
2023-06-20 23:14:37 +0800

    chore: add MallAdminApplication

---
+++ backend-server/mall-admin/src/main/java/com/shunyue/mall/MallAdminApplication.java
@@ -0,0 +1,12 @@
+package com.shunyue.mall;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MallAdminApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MallAdminApplication.class, args);
+    }
+}
---
+++ backend-server/mall-admin/src/main/java/com/shunyue/mall/controller/UmsAdminController.java
@@ -0,0 +1,13 @@
+package com.shunyue.mall.controller;
+
+import io.swagger.annotations.Api;
+import org.springframework.stereotype.Controller;
+
+@Controller
+@Api(tags = "UmsAdminController")
+@Tag(name = "UmsAdminController", description = "后台用户管理")
+
+@RequestMapping("/admin")
+public class UmsAdminController {
+
+}
