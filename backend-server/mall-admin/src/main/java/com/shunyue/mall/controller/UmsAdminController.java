c1d6bae75ea8a8ec55781f4c54ebb77229264804
shunyue1320 <tsunyue1320@gmail.com>
2023-06-24 22:31:47 +0800

    chore: add register

--- backend-server/mall-admin/src/main/java/com/shunyue/mall/controller/UmsAdminController.java
+++ backend-server/mall-admin/src/main/java/com/shunyue/mall/controller/UmsAdminController.java
@@ -1,22 +1,36 @@
 package com.shunyue.mall.controller;
 
 import com.shunyue.mall.common.api.CommonResult;
+import com.shunyue.mall.model.UmsAdmin;
+import com.shunyue.mall.dto.UmsAdminParam;
+import com.shunyue.mall.service.UmsAdminService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @Api(tags = "UmsAdminController")
 @Tag(name = "UmsAdminController", description = "后台用户管理")
 @RequestMapping("/admin")
 public class UmsAdminController {
+    @Autowired
+    private UmsAdminService adminService;
 
-//    @ApiOperation(value = "用户注册")
-//    @RequestMapping(value = "/register", method = RequestMethod.POST)
-//    @RequestBody
-//    public CommonResult<UmsAdmin> reg
+    @ApiOperation(value = "用户注册")
+    @RequestMapping(value = "/register", method = RequestMethod.POST)
+    @ResponseBody
+    public CommonResult<UmsAdmin> register(@Validated @RequestBody UmsAdminParam umsAdminParam) {
+        UmsAdmin umsAdmin = adminService.register(umsAdminParam);
+        if (umsAdmin == null) {
+            return CommonResult.failed();
+        }
+        return CommonResult.success(umsAdmin);
+    }
 }
---
+++ backend-server/mall-admin/src/main/java/com/shunyue/mall/service/impl/UmsAdminServiceImpl.java
@@ -0,0 +1,30 @@
+package com.shunyue.mall.service.impl;
+
+import com.shunyue.mall.dto.UmsAdminParam;
+import com.shunyue.mall.model.UmsAdmin;
+import org.springframework.beans.BeanUtils;
+
+import java.util.Date;
+import java.util.List;
+
+public class UmsAdminServiceImpl {
+    @Override
+    public UmsAdmin register(UmsAdminParam umsAdminParam) {
+        UmsAdmin umsAdmin = new UmsAdmin();
+        BeanUtils.copyProperties(umsAdminParam, umsAdmin);
+        umsAdmin.setCreateTime(new Date());
+        umsAdmin.setStatus(1);
+        //查询是否有相同用户名的用户
+        UmsAdminExample example = new UmsAdminExample();
+        example.createCriteria().andUsernameEqualTo(umsAdmin.getUsername());
+        List<UmsAdmin> umsAdminList = adminMapper.selectByExample(example);
+        if (umsAdminList.size() > 0) {
+            return null;
+        }
+        //将密码进行加密操作
+        String encodePassword = passwordEncoder.encode(umsAdmin.getPassword());
+        umsAdmin.setPassword(encodePassword);
+        adminMapper.insert(umsAdmin);
+        return umsAdmin;
+    }
+}
