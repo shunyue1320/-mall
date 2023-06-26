95d1b6c27125c9019c0083da36e55db285c6a8b2
shunyue1320 <tsunyue1320@gmail.com>
2023-06-26 22:51:39 +0800

    chore: 用户id 与 角色id(后台管理系统路由表) 的表映射

--- backend-server/mall-mbg/src/main/java/com/shunyue/mall/model/UmsRole.java
+++ backend-server/mall-mbg/src/main/java/com/shunyue/mall/model/UmsRole.java
@@ -5,26 +5,25 @@ import io.swagger.annotations.ApiModelProperty;
 import java.io.Serializable;
 import java.util.Date;
 
-/**
- * 后台管理系统路由表
- */
-public class UmsResource implements Serializable {
+public class UmsRole implements Serializable {
     private Long id;
 
-    @ApiModelProperty(value = "创建时间")
-    private Date createTime;
-
-    @ApiModelProperty(value = "资源名称")
+    @ApiModelProperty(value = "名称")
     private String name;
-
-    @ApiModelProperty(value = "资源URL")
-    private String url;
 
     @ApiModelProperty(value = "描述")
     private String description;
 
-    @ApiModelProperty(value = "资源分类ID")
-    private Long categoryId;
+    @ApiModelProperty(value = "后台用户数量")
+    private Integer adminCount;
+
+    @ApiModelProperty(value = "创建时间")
+    private Date createTime;
+
+    @ApiModelProperty(value = "启用状态：0->禁用；1->启用")
+    private Integer status;
+
+    private Integer sort;
 
     private static final long serialVersionUID = 1L;
 
@@ -36,28 +35,12 @@ public class UmsResource implements Serializable {
         this.id = id;
     }
 
-    public Date getCreateTime() {
-        return createTime;
-    }
-
-    public void setCreateTime(Date createTime) {
-        this.createTime = createTime;
-    }
-
     public String getName() {
         return name;
     }
 
     public void setName(String name) {
         this.name = name;
-    }
-
-    public String getUrl() {
-        return url;
-    }
-
-    public void setUrl(String url) {
-        this.url = url;
     }
 
     public String getDescription() {
@@ -68,12 +51,36 @@ public class UmsResource implements Serializable {
         this.description = description;
     }
 
-    public Long getCategoryId() {
-        return categoryId;
+    public Integer getAdminCount() {
+        return adminCount;
     }
 
-    public void setCategoryId(Long categoryId) {
-        this.categoryId = categoryId;
+    public void setAdminCount(Integer adminCount) {
+        this.adminCount = adminCount;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+
+    public Integer getSort() {
+        return sort;
+    }
+
+    public void setSort(Integer sort) {
+        this.sort = sort;
     }
 
     @Override
@@ -83,11 +90,12 @@ public class UmsResource implements Serializable {
         sb.append(" [");
         sb.append("Hash = ").append(hashCode());
         sb.append(", id=").append(id);
-        sb.append(", createTime=").append(createTime);
         sb.append(", name=").append(name);
-        sb.append(", url=").append(url);
         sb.append(", description=").append(description);
-        sb.append(", categoryId=").append(categoryId);
+        sb.append(", adminCount=").append(adminCount);
+        sb.append(", createTime=").append(createTime);
+        sb.append(", status=").append(status);
+        sb.append(", sort=").append(sort);
         sb.append(", serialVersionUID=").append(serialVersionUID);
         sb.append("]");
         return sb.toString();
