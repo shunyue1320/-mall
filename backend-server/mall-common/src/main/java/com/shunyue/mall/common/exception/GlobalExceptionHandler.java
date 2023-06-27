d6ea433337e331837dd55297e6032907bc956a16
shunyue1320 <tsunyue1320@gmail.com>
2023-06-27 23:09:15 +0800

    chore: 添加  ControllerAdvice 全局错误处理类

---
+++ backend-server/mall-common/src/main/java/com/shunyue/mall/common/exception/GlobalExceptionHandler.java
@@ -0,0 +1,82 @@
+package com.shunyue.mall.common.exception;
+
+
+import cn.hutool.core.util.StrUtil;
+import com.shunyue.mall.common.api.CommonResult;
+import org.springframework.validation.BindException;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.sql.SQLSyntaxErrorException;
+
+
+/**
+ * 全局错误处理类
+ */
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    /**
+     * 捕获 ApiException 异常
+     * @param e
+     * @return
+     */
+    @ExceptionHandler(ApiException.class)
+    @ResponseBody
+    public CommonResult handle(ApiException e) {
+        if (e.getErrorCode() != null) {
+            return CommonResult.failed(e.getErrorCode());
+        }
+        return CommonResult.failed(e.getMessage());
+    }
+
+    /**
+     * 捕获方法参数无效异常
+     * @param e
+     * @return
+     */
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseBody
+    public CommonResult handleValidException(MethodArgumentNotValidException e) {
+        BindingResult bindingResult = e.getBindingResult();
+        String message = null;
+        if (bindingResult.hasErrors()) {
+            FieldError fieldError = bindingResult.getFieldError();
+            if (fieldError != null) {
+                message = fieldError.getField() + fieldError.getDefaultMessage();
+            }
+        }
+
+        return CommonResult.validateFailed(message);
+    }
+
+
+    @ExceptionHandler(BindException.class)
+    @ResponseBody
+    public CommonResult handleValidException(BindException e) {
+        BindingResult bindingResult = e.getBindingResult();
+        String message = null;
+        if (bindingResult.hasErrors()) {
+            FieldError fieldError = bindingResult.getFieldError();
+            if (fieldError != null) {
+                message = fieldError.getField()+fieldError.getDefaultMessage();
+            }
+        }
+        return CommonResult.validateFailed(message);
+    }
+
+
+    @ResponseBody
+    @ExceptionHandler(value = SQLSyntaxErrorException.class)
+    public CommonResult handleSQLSyntaxErrorException(SQLSyntaxErrorException e) {
+        String message = e.getMessage();
+        if (StrUtil.isNotEmpty(message) && message.contains("denied")) {
+            message = "演示环境暂无修改权限，如需修改数据可本地搭建后台服务！";
+        }
+        return CommonResult.failed(message);
+    }
+}
