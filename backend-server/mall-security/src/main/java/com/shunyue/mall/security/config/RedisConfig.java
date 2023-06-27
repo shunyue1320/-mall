639853593f6b1588019b1b873cc47c9d67587ef9
shunyue1320 <tsunyue1320@gmail.com>
2023-06-27 08:21:49 +0800

    chore: 实现 Redis 配置类

---
+++ backend-server/mall-security/src/main/java/com/shunyue/mall/security/config/RedisConfig.java
@@ -0,0 +1,15 @@
+package com.shunyue.mall.security.config;
+
+import com.shunyue.mall.common.config.BaseRedisConfig;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Redis配置类
+ * Created by macro on 2020/3/2.
+ */
+@EnableCaching
+@Configuration
+public class RedisConfig extends BaseRedisConfig {
+
+}
