cf6b9eefa25fbcaae2e81863304bafb75ef99d70
shunyue1320 <tsunyue1320@gmail.com>
2023-06-30 17:01:58 +0800

    chore: 语言状态管理

---
+++ web/src/store/modules/locale.ts
@@ -0,0 +1,60 @@
+import { useCache } from '@/hooks/web/useCache'
+import zhCn from 'element-plus/es/locale/lang/zh-cn'
+import en from 'element-plus/es/locale/lang/en'
+import { defineStore } from 'pinia'
+import { LocaleDropdownType } from '@/types/localeDropdown'
+import { store } from '../index'
+
+const { wsCache } = useCache()
+
+const elLocaleMap = {
+  'zh-CN': zhCn,
+  en: en
+}
+
+interface LocaleState {
+  currentLocale: LocaleDropdownType
+  localeMap: LocaleDropdownType[]
+}
+
+export const useLocaleStore = defineStore('locales', {
+  state: (): LocaleState => {
+    const currentLang: LocaleType = wsCache.get('lang')
+
+    return {
+      currentLocale: {
+        lang: currentLang || 'zh-CN',
+        elLocale: elLocaleMap[currentLang || 'zh-CN']
+      },
+      localeMap: [
+        {
+          lang: 'zh-CN',
+          name: '简体中文'
+        },
+        {
+          lang: 'en',
+          name: 'English'
+        }
+      ]
+    }
+  },
+  getters: {
+    getCurrentLocale(): LocaleDropdownType {
+      return this.currentLocale
+    },
+    getLocaleMap(): LocaleDropdownType[] {
+      return this.localeMap
+    }
+  },
+  actions: {
+    setCurrentLocale(lang: LocaleType): void {
+      this.currentLocale.lang = lang
+      this.currentLocale.elLocale = elLocaleMap[lang]
+      wsCache.set('lang', lang)
+    }
+  }
+})
+
+export const useLocaleStoreWithOut = () => {
+  return useLocaleStore(store)
+}
