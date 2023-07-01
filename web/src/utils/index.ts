c220376231ca2cf9af1cad57b50a1680c3098af2
shunyue1320 <tsunyue1320@gmail.com>
2023-07-01 10:00:57 +0800

    chore: 中划线和驼峰字符串相互转换方法

---
+++ web/src/utils/index.ts
@@ -0,0 +1,16 @@
+/**
+ * @param str 需要转中划线的驼峰字符串
+ * @returns 字符串下划线
+ */
+export const humpToUnderline = (str: string): string => {
+  return str.replace(/([A-Z])/g, '-$1').toLowerCase()
+}
+
+/**
+ * @param str 需要转驼峰的中划线字符串
+ * @returns 字符串驼峰
+ */
+export const underlineToHump = (str: string): string => {
+  if (!str) return ''
+  return str.replace(/\-(w)/g, (_, letter: string) => letter.toUpperCase())
+}
